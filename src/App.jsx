7aa201625aeaba56f9f05a530a44c7fc7ddff688
lemuelpires <lemuelpires@gmail.com>
2024-05-15 22:16:22 -0300

    ajustes de código, importacao

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './containers/Home';
+import AmbienteAdministrador from './containers/AmbienteAdministrador';
+import AmbienteUsuario from './containers/AmbienteUsuario';
+import CadastroAnuncios from './containers/CadastroAnuncios';
+import CadastroEndereco from './containers/CadastroEndereco';
+import CadastroJogos from './containers/CadastroJogos';
+import CadastroProdutos from './containers/CadastroProdutos';
+import CadastroUsuarios from './containers/CadastroUsuarios';
+import CarrinhoCompras from './containers/CarrinhoCompras';
+import HomeListagem from './containers/HomeListagem';
+import Login from './containers/Login';
+import ListaUsuarios from './containers/ListaUsuarios';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
 
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  <Router>
+    <Header />
+      <div className='container'>
+        <Routes>
+        <Route path='/' element={<Home />} />
+              <Route path='/ambiente-administrador' element={<AmbienteAdministrador />} />
+              <Route path='/ambiente-usuario' element={<AmbienteUsuario />} />
+              <Route path='/cadastro-anuncios' element={<CadastroAnuncios />} />
+              <Route path='/cadastro-endereco' element={<CadastroEndereco />} />
+              <Route path='/cadastro-jogos' element={<CadastroJogos />} />
+              <Route path='/cadastro-produtos' element={<CadastroProdutos />} />
+              <Route path='/cadastro-usuarios' element={<CadastroUsuarios />} />
+              <Route path='/carrinho-compras' element={<CarrinhoCompras />} />
+              <Route path='/home-listagem' element={<HomeListagem />} />
+              <Route path='/login' element={<Login />} />
+              <Route path='/lista-usuarios' element={<ListaUsuarios />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Footer />
+</Router>
+  );
 }
 
-export default App
+export default App;

6dfd189e1a2e432fcfc220aa298346b06ba597f0
lemuelpires <lemuelpires@gmail.com>
2024-06-12 19:29:30 -0300

    ajuste importação

--- src/components/AnuncioSetupGamer/index.jsx
+++ src/components/AnuncioSetupGamer/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ContainerSetup, ContainerTexto, ContainerButton, ContainerImagem, ContainerSuperior  } from "./style";
 import BCliqueAqui from "../Button/CliqueAqui";
-import ImagemSetupGamer2 from "../../assets/ImagemSetupGamer2.svg";
+import ImagemSetupGamer2 from "../../assets/imagemSetupGamer2.svg";
 
 function AnuncioSetupGamer() {
     return (

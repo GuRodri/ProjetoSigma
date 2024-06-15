6ed0c7a7c2514d19a7ecb69131605d01a09d2774
lemuelpires <lemuelpires@gmail.com>
2024-06-14 23:06:13 -0300

    ajuste no menu hamburguer

--- src/components/MenuHamburguer/index.jsx
+++ src/components/MenuHamburguer/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+/*import React, { useState, useEffect, useRef } from 'react';
 import { FaBars } from 'react-icons/fa'; // Ícone de menu hambúrguer do react-icons
 import { Container, MenuIcon, MenuOptions, UserName } from './style'; // Importe UserName do seu estilo
 import { NavLink } from 'react-router-dom';
@@ -59,4 +59,68 @@ const HamburgerMenu = () => {
   );
 };
 
+export default HamburgerMenu;*/
+import React, { useState, useEffect, useRef } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa'; // Ícone de fechar do react-icons
+import { Container, MenuIcon, MenuOptions, CloseButton } from './style'; // Adicione CloseButton ao seu estilo
+import { NavLink } from 'react-router-dom';
+import { useAuth } from '../../context/autContexto1';
+
+const HamburgerMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const { currentUser } = useAuth();
+  const menuRef = useRef(null);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const closeMenu = () => {
+    setIsOpen(false);
+  };
+
+  const handleMouseLeave = () => {
+    closeMenu();
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener('mouseleave', handleMouseLeave);
+    } else {
+      document.removeEventListener('mouseleave', handleMouseLeave);
+    }
+
+    return () => {
+      document.removeEventListener('mouseleave', handleMouseLeave);
+    };
+  }, [isOpen]);
+
+  return (
+    <Container>
+      <MenuIcon onClick={toggleMenu}>
+        <FaBars />
+      </MenuIcon>
+      <MenuOptions open={isOpen} ref={menuRef} onMouseLeave={handleMouseLeave}>
+        <CloseButton onClick={closeMenu}>
+          <FaTimes />
+        </CloseButton>
+        {currentUser && currentUser.role === 0 && (
+          <>
+            <NavLink to="/ambiente-usuario" onClick={closeMenu}>Perfil Usuário</NavLink>
+            <NavLink to="/favoritos" onClick={closeMenu}>Favoritos</NavLink>
+            <NavLink to="/historico-de-compras" onClick={closeMenu}>Histórico de Compras</NavLink>
+          </>
+        )}
+        {currentUser && currentUser.role === 1 && (
+          <>
+            <NavLink to="/ambiente-administrador" onClick={closeMenu}>Perfil Administrador</NavLink>
+          </>
+        )}
+        <NavLink to="/sobre-nos" onClick={closeMenu}>Sobre Nós</NavLink>
+      </MenuOptions>
+    </Container>
+  );
+};
+
 export default HamburgerMenu;
+
--- src/components/MenuHamburguer/style.js
+++ src/components/MenuHamburguer/style.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+/*import styled from 'styled-components';
 
 export const Container = styled.div`
   position: relative;
@@ -20,6 +20,11 @@ export const MenuOptions = styled.div`
   box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
   z-index: 1;
   border-radius: 0.5em;
+  width:200px;
+  height:77.8vh;
+  border-bottom: 1px solid #141516;
+
+  
 
   a {
     color: d9d9d9;
@@ -32,6 +37,57 @@ export const MenuOptions = styled.div`
     }
   }
 `;
+export const UserName = styled.span`
+  color: #d9d9d9;
+`;*/
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+export const MenuIcon = styled.div`
+  cursor: pointer;
+  font-size: 28px;
+  color: #d9d9d9;
+`;
+
+export const MenuOptions = styled.div`
+  display: ${({ open }) => (open ? 'block' : 'none')};
+  position: fixed;
+  top: 0;
+  right: 0;
+  background-color: white;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+  width: 300px;
+  height: 100vh;
+  border-radius: 0 0 0 0.5em;
+  border-bottom: 1px solid #141516;
+  padding-top: 40px;
+
+  a {
+    color: black;
+    padding: 12px 16px;
+    text-decoration: none;
+    display: block;
+
+    &:hover {
+      background-color: #ddd;
+    }
+  }
+`;
+
+export const CloseButton = styled.div`
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+  font-size: 24px;
+  color: black;
+`;
+
 export const UserName = styled.span`
   color: #d9d9d9;
 `;

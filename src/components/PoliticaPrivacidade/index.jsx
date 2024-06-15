7c08ae27f0210390d5cf00d56aee2f8adf2694a3
lemuelpires <lemuelpires@gmail.com>
2024-06-15 18:34:25 -0300

    adicionada a funcionalidade de termos de serviço e politica de privacidade,

---
+++ src/components/PoliticaPrivacidade/index.jsx
@@ -0,0 +1,167 @@
+import React from 'react';
+import { ContainerPoliticaPrivacidade} from './style';
+
+const PoliticaPrivacidade = () => {
+    return (
+        
+        <ContainerPoliticaPrivacidade>
+          <h2>Política de Privacidade</h2>
+          <p>
+            Nesta Política de Privacidade, vamos explicar aos nossos visitantes e usuários, todas as formas e meios que protegemos e tratamos os dados coletados em nossa plataforma.
+          </p>
+          <p>
+            Nós levamos extremamente a sério a proteção dos dados pessoais coletados e armazenados, e neste documento, vamos te explicar detalhadamente como e porque fazemos esta coleta, e como você pode pedir para retirar, alterar ou excluir seus dados de nossos servidores.
+          </p>
+          <p>
+            Em caso de dúvidas de quaisquer tipos, você pode entrar em contato diretamente com nossa equipe pela aba de contato.
+          </p>
+          <p>
+            Este documento foi criado pela Advogada Livia Martins Fioraneli (OAB/SP xxx.xxx), e adaptado para utilização neste website.
+          </p>
+      
+          <h3>1 – Sobre a Origem dos seus dados Pessoais</h3>
+          <p>
+            Nosso site pode coletar alguns dados pessoais seus para diferentes objetivos. Para coletarmos estes dados, usamos as seguintes tecnologias:
+          </p>
+          <ul>
+            <li>
+              <strong>Website:</strong> Dentro do nosso site você poderá preencher formulários, enviar comentários e dúvidas, entrar em contato com nossa equipe ou se cadastrar na nossa plataforma, e seus dados serão salvos para posterior atendimento.
+            </li>
+            <li>
+              <strong>Mensagens e comunicações:</strong> Quando você se comunica com nossa equipe, seja pelo Whatsapp, por SMS, por E-mail ou quaisquer outros meios alguns dados podem ficar salvos para futura conferência e facilitar a comunicação com nossos clientes e usuários.
+            </li>
+            <li>
+              <strong>De Modo Offline:</strong> Alguns dados podem ser coletados offline, como, por exemplo, em eventos e organizações, onde podemos coletar seu e-mail ou outros dados, para manter comunicação e informações necessárias que você precisa ter conhecimento.
+            </li>
+            <li>
+              <strong>Dados de Terceiros:</strong> Algumas vezes, terceiros podem enviar para nossa empresa alguns dados seus, como, por exemplo, dados recebidos por mídias sociais (como Facebook, Instagram, TikTok ou outras redes sociais), do governo ou intermediadoras de pagamentos.
+            </li>
+          </ul>
+      
+          <h3>2 – Como e porque coletamos seus dados:</h3>
+          <p>
+            É importante que você, nosso usuário ou visitante, saiba como e quais dados pessoais coletamos e utilizamos quando você visita nosso site. Destacamos que sempre tentamos coletar o mínimo de dados possíveis para sua maior segurança, porém, alguns dados são essenciais para podermos entregar o melhor serviço possível e até para o funcionamento do site, conforme descrevemos abaixo:
+          </p>
+          <ul>
+            <li>
+              <strong>Marketing:</strong> Quando você preenche formulários e se cadastra no site, você poderá receber e-mails com promoções e novidades do site. Podemos coletar nome, e-mail e telefone.
+            </li>
+            <li>
+              <strong>Navegação ao Site:</strong> Quando você navega pelo nosso site, coletamos automaticamente dados como o seu endereço IP, tipo de navegador, páginas acessadas, tempo gasto em cada página e outras informações.
+            </li>
+            <li>
+              <strong>Informações de contato:</strong> Quando você entra em contato conosco, seja por e-mail, telefone ou chat, coletamos as suas informações de contato, como o seu nome, e-mail, número de telefone e a mensagem que você nos enviou.
+            </li>
+            <li>
+              <strong>Informações de uso dos serviços:</strong> Quando você usa os nossos serviços, coletamos dados sobre as suas atividades, como as pesquisas que você realiza, os vídeos que você assiste, as páginas que você visita e as compras que você faz.
+            </li>
+            <li>
+              <strong>Informações de localização:</strong> Quando você usa os nossos serviços, coletamos dados de localização precisos para fornecer os serviços que você solicitou.
+            </li>
+          </ul>
+      
+          <h3>3 – Sobre o uso de Cookies</h3>
+          <p>
+            Usamos cookies no nosso website e aplicativos para melhorar a sua experiência de navegação. Cookie é um pequeno arquivo de texto que um site armazena no seu computador ou dispositivo móvel quando você o acessa/visita.
+          </p>
+          <p>
+            Os cookies nos permitem reconhecer o seu navegador e fornecer-lhe a melhor experiência ao navegar no nosso site. Além disso, os cookies nos ajudam a compreender quais seções do site são mais interessantes para você e quais conteúdos podem ser recomendados para você, site com o google, bing ou https://gpabrasil.com.br/, utilizam cookies, e ao visitar cada um, você deve verificar a política referente a eles.
+          </p>
+          <p>
+            Você pode a qualquer momento bloquear os cookies no seu navegador, ou limpar o cache para retirar os mesmos.
+          </p>
+          <p>
+            É importante destacar que tentamos minimizar o uso de cookies não essenciais, mas utilizamos os necessários para entregar um melhor serviço, conforme a previsão de legítimo interesse na LGPD. Você pode a qualquer momento desativar os mesmos em seu navegador.
+          </p>
+      
+          <h3>4 – Com quem compartilhamos os seus dados pessoais:</h3>
+          <p>
+            <strong>Prestadores de Serviços:</strong> compartilhamos os seus dados pessoais com os nossos prestadores de serviços, que prestam serviços de marketing, análise de dados, pagamentos, envio de e-mails e outros serviços para nós. Esses prestadores de serviços só podem usar os seus dados pessoais para realizar esses serviços para nós e conforme as nossas instruções.
+          </p>
+          <p>
+            <strong>Anunciantes e Parceiros Comerciais:</strong> também é possível que compartilhemos os seus dados pessoais com empresas que realizem anúncios ou sejam parceiras comerciais. No entanto, só compartilhamos os seus dados pessoais conforme as suas instruções.
+          </p>
+          <p>
+            <strong>Comunidades Online:</strong> quando você se junta a uma das nossas comunidades online, os seus dados pessoais podem ser vistos por outros membros dessa comunidade.
+          </p>
+          <p>
+            <strong>Investidores e Filiais:</strong> podemos compartilhar os seus dados pessoais com os nossos investidores e filiais. No entanto, só compartilhamos os seus dados pessoais conforme as suas instruções.
+          </p>
+          <p>
+            <strong>Autoridades Governamentais e outras Partes:</strong> às vezes, podemos ser obrigados judicialmente a liberar ou divulgar os seus dados às autoridades governamentais ou outras partes, conforme exigido por lei ou por outros motivos legais.
+          </p>
+      
+          <h3>5 – Sobre seus Direitos em relação aos Dados coletados</h3>
+          <p>
+            O presente texto tem por objetivo esclarecer os nossos leitores e visitantes sobre quais são os seus direitos em relação aos dados pessoais.
+          </p>
+          <p>
+            De acordo com a Lei Geral de Proteção de Dados Pessoais (LGPD), toda pessoa tem o direito à informação, à transparência, à liberdade e à autonomia na tomada de decisões.
+          </p>
+          <p>
+            Com relação aos dados pessoais, a pessoa tem o direito ao acesso, à correção, à eliminação, à portabilidade, à limitação do tratamento e à oposição.
+          </p>
+          <p>
+            A pessoa também tem o direito de negar a qualquer momento que seus dados pessoais sejam utilizados para fins de marketing e ao recebimento de comunicações comerciais.
+          </p>
+          <p>
+            Para exercer os seus direitos, basta entrar em contato com a nossa equipe. Esclarecemos que os dados pessoais são tratados de forma confidencial e que tomamos todas e quaisquer medidas de segurança necessárias para garantir a proteção dos dados.
+          </p>
+      
+          <h3>6 – Sobre a segurança que aplicamos no tratamento dos seus dados</h3>
+          <p>
+            A segurança é um dos pilares fundamentais da nossa política de privacidade.
+          </p>
+          <p>
+            Todos os dados pessoais que você fornecer ao site serão tratados com total confidencialidade e segurança, de acordo com as normas legais aplicáveis.
+          </p>
+          <p>
+            Para garantir a proteção dos seus dados pessoais, o site utiliza diversas medidas de segurança, como criptografia SSL (Secure Socket Layer), firewalls, sistemas de detecção de invasões, monitoramento constante das redes, uso de senhas seguras e verificação periódica da segurança dos sistemas.
+          </p>
+          <p>
+            Todas as informações pessoais são armazenadas em servidores seguros, protegidos contra acessos não autorizados, utilizando os mais modernos e avançados sistemas de segurança.
+          </p>
+          <p>
+            Nossa equipe de segurança está constantemente monitorando os sistemas e atualizando as medidas de proteção, para garantir que seus dados estejam sempre seguros. Caso você tenha alguma dúvida ou preocupação em relação à segurança dos seus dados pessoais, entre em contato conosco.
+          </p>
+      
+          <h3>7 – Pedido de modificação, remoção ou alteração</h3>
+          <p>
+            Entendemos que você possa, eventualmente, querer modificar, remover ou alterar seus dados pessoais.
+          </p>
+          <p>
+            Se você deseja fazer uma alteração, uma remoção ou uma modificação, entre em contato conosco e nós faremos o possível para atender à sua solicitação. No entanto, tenha em mente que, em alguns casos, pode não ser possível atender à sua solicitação, especialmente se for necessária para cumprir nossas obrigações legais ou contratuais.
+          </p>
+          <p>
+            Além disso, lembre-se de que, se você solicitar a remoção de seus dados pessoais do nossos banco de dados, isso pode afetar o uso de nosso site e serviços. Se você não puder fazer uma alteração ou uma modificação, ou se desejar excluir seus dados pessoais, entre em contato conosco para podermos discutir outras opções, clicando na aba contato.
+          </p>
+      
+          <p>
+            O Modelo abaixo já é recomendado para empresas que possuem diferentes aplicações e tem a equipe jurídica e técnica para preencher e conectar todos os pontos dos documentos.
+          </p>
+          <p>
+            Para maioria dos sites, recomenda-se utilizar a versão anterior.
+          </p>
+      
+          <p>
+            <strong>Modelo de Política de privacidade e segurança (Versão Estendida)</strong>
+          </p>
+          <p>
+            Conforme as normas previstas na Lei 13.709/18, que versa sobre a proteção de dados e as demais legislações pertinentes, apresentamos a Política de Privacidade e Segurança do (coloque o nome do site/blog aqui).
+          </p>
+          <p>
+            Este documento é vinculado e integra as Condições e Termos de Uso deste site, sendo passível de alteração a qualquer momento para adequação em regras estabelecidas pelas legislações que estabelecem o modo de tratamento dos dados.
+          </p>
+          <p>
+            O acesso, visita e permanência neste site/blog é de livre escolha e acontece conforme a vontade/desejo do usuário, sendo sua visita e prosseguimento no site, escolha deste usuário.
+          </p>
+          <p>
+            Ficando ciente por meio deste documento que a partir do momento em que mantém o acesso e a permanência em qualquer uma das nossas páginas está automaticamente demonstrando estar de acordo, de forma explícita com todas as regras e termos trazidos na Política de Privacidade e Segurança descrita abaixo.
+          </p>
+          <p>
+            Sendo seu direito a qualquer momento entrar em contato conosco para maiores informações do que aqui foi descrito, bem como esclarecimento de dúvidas que possam vir a existir.
+          </p>
+        </ContainerPoliticaPrivacidade>
+      );
+} 
+export default PoliticaPrivacidade;
---
+++ src/components/TermosUso/style.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  font-family: Arial, sans-serif;
+  line-height: 1.6;
+  color: #333;
+  margin: 20px;
+`;
+
+// Estilos para títulos
+export const Title = styled.h2`
+  font-size: 1.5em;
+  color: #007bff;
+  margin: 20px 0 10px;
+`;
+
+// Estilo para textos normais
+export const Text = styled.p`
+  margin-bottom: 15px;
+`;
+
+// Estilo para listas
+export const List = styled.ul`
+  margin-bottom: 15px;
+`;
+
+// Estilo para itens de lista
+export const ListItem = styled.li`
+  margin-left: 20px;
+`;

cf8e70bd366a60c6bb8b543a912cd8ff1ba5f9a3
lemuelpires <lemuelpires@gmail.com>
2024-06-18 00:52:27 -0300

    atualização de rotas e instalação de pacotes

--- src/App.jsx
+++ src/App.jsx
@@ -37,6 +37,15 @@ import DetalhesJogo from './containers/DetalheJogo';
 import CadastroImagens from './containers/CadastroImagens';
 import ListaImagens from './containers/ListaImagens';
 import RecuperarSenha from './containers/RecuperarSenha';
+import Produto from './containers/Produto';
+import SobreNos from './containers/SobreNos';
+import AvaliacaoProduto from './components/AvaliacaoProduto';
+import SetupGamerPage1 from './containers/SetupGamer/Page1';
+import SetupGamerPage2 from './containers/SetupGamer/Page2';
+import SetupGamerPage3 from './containers/SetupGamer/Page3';
+import SetupGamerPage4 from './containers/SetupGamer/Page4';
+import SetupGamerPage5 from './containers/SetupGamer/Page5';
+import SetupGamerPage6 from './containers/SetupGamer/Page6';
 
 // Configuração do elemento da aplicação para o Modal
 Modal.setAppElement('#root');
@@ -54,7 +63,17 @@ const App = () => {
                 <Route path='/ambiente-usuario' element={<AmbienteUsuario />} />
                 <Route path='/cadastro-usuarios' element={<CadastroUsuarios />} />
                 <Route path='/home-listagem' element={<HomeListagem />} />
+                <Route path="/produto/:id" element={<Produto />} />
+                <Route path='/produto/:id/avaliacao' element={<AvaliacaoProduto />} />
+                <Route path='/produto' element={<Produto />} />
                 <Route path='/recuperar-senha' element={<RecuperarSenha />} />
+                <Route path='/sobre-nos' element={<SobreNos />} />
+                <Route path='/setupGamer-page1' element={<SetupGamerPage1 />} />
+                <Route path='/setupGamer-page2' element={<SetupGamerPage2 />} />
+                <Route path='/setupGamer-page3' element={<SetupGamerPage3 />} />
+                <Route path='/setupGamer-page4' element={<SetupGamerPage4 />} />
+                <Route path='/setupGamer-page5' element={<SetupGamerPage5 />} />
+                <Route path='/setupGamer-page6' element={<SetupGamerPage6 />} />
                 <Route path='/carrinho-compras' element={<ProtectedRoute element={<CarrinhoCompras />} requiredRole={0} />} />
                 <Route path='/favoritos' element={<ProtectedRoute element={<Favoritos />} requiredRole={0} />} />
                 <Route path='/login' element={<Login />} />
